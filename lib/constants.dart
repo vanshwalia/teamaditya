6d40c003dcb8835dac50cde81e9eca4692335eec
Aditya Thakur <adityathakurxd@users.noreply.github.com>
2021-06-27 19:30:30 +0530

    Repurposed files

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color kPrimaryRed = Color(0xFFDA0037);
+Color kPrimaryWhite = Color(0xFFEDEDED);
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,56 +1,51 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
+    return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                  'Team',
-                  style: GoogleFonts.montserrat(fontSize: 40.0)
-              ),
-              SizedBox(
-                width: 20.0,
-              ),
-              Text(
-                  'Aditya',
-                  style: GoogleFonts.merienda(fontSize: 40.0,fontWeight: FontWeight.w600)
-              ),
-            ],
-          ),
-          bottom: TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.message), text: 'Discord',),
-              Tab(icon: Icon(Icons.video_call), text: 'YouTube',),
-            ],
-          ),
-        ),
-        body: TabBarView(
+        body: Column(
           children: [
-            Column(
+            const SizedBox(
+              width: 30,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Container(
-                  color: Colors.white,
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  height: 150,
-                )
+                const SizedBox(
+                  width: 20,
+                ),
+                SelectableText(
+                  'Team Aditya',
+                  style: GoogleFonts.montserrat(
+                      fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+                Spacer(),
+                Text(
+                  'Discord',
+                  style: GoogleFonts.montserrat(fontSize: 20),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  'YouTube',
+                  style: GoogleFonts.montserrat(fontSize: 20),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
               ],
             ),
-            Icon(Icons.directions_transit),
+            Container(
+
+
+            )
           ],
         ),
       ),
